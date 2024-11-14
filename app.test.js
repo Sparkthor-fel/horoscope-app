d7d4c59426e1dd21ab44529a04e59cf8f5587b47
Felicien <felicien.auguin@wanadoo.fr>
2024-11-14 12:18:24 +0100

    Corrected all tests

--- app.test.js
+++ app.test.js
@@ -1,45 +1,42 @@
 const request = require("supertest");
 const app = require("./app");
 
-afterAll(() => {
-   app.close();
-});
 
 describe("GET /horoscope", () => {
 	it("Invalid data lenght", async () => {
 		const res = await request(app)
 			.get("/horoscope")
-			.send({
+			.query({
 				bday: "invalid",
 			});
 		expect(res.statusCode).toEqual(400);
-		expect(res.body.message).toEqual("Invalid date format : pls send according yyyy-mm-dd");
+		expect(res.text).toEqual("Invalid date format : pls send according yyyy-mm-dd");
 	});
 	
 	it("Invalid date format", async () => {
 		const res = await request(app)
 			.get("/horoscope")
-			.send({
+			.query({
 				bday: "1991-in-ds",
 			});
 		expect(res.statusCode).toEqual(400);
-		expect(res.body.message).toEqual("Error while parsing date : pls send according yyyy-mm-dd");
+		expect(res.text).toEqual("Error while parsing date : pls send according yyyy-mm-dd");
 	});
 	
 	it("Invalid date", async () => {
 		const res = await request(app)
 			.get("/horoscope")
-			.send({
+			.query({
 				bday: "1991-50-50",
 			});
 		expect(res.statusCode).toEqual(400);
-		expect(res.body.message).toEqual("Invalid date entered");
+		expect(res.text).toEqual("Invalid date entered");
 	});
 	
 	it("Valid", async () => {
 		const res = await request(app)
 			.get("/horoscope")
-			.send({
+			.query({
 				bday: "1991-01-19",
 			});
 		expect(res.statusCode).toEqual(200);
