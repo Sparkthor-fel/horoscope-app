9477273562a439fa9035cde9020adfbddefa4972
Felicien <felicien.auguin@wanadoo.fr>
2024-11-14 10:37:08 +0100

    Fix app.js file

--- app.js
+++ app.js
@@ -34,8 +34,7 @@ app.get('/horoscope', (req, res) => {
 
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server is Successfully Running, 
-                   and App is listening on port "+ PORT)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
     else 
         console.log("Error occurred, server can't start", error);
     }
